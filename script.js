6c9cfcfc55b4fcbdee18fd1dca279e42cd442905
praveenkumarpadamati <129651229+praveenkumarpadamati@users.noreply.github.com>
2023-05-01 13:46:01 +0530

    First Version 1 created
    Here I have created a Temperature Converter. This converts the temperature from celsius to fahrenheit and kelvin  and viceversa.

---
+++ script.js
@@ -0,0 +1,22 @@
+let celsius=document.getElementById('celsius')
+let fahrenheit=document.getElementById('fahrenheit')
+let kelvin=document.getElementById('kelvin')
+celsius.oninput=() =>{
+    let output1=(parseFloat(celsius.value)*9)/5+32
+    fahrenheit.value=parseFloat(output1.toFixed(2));
+    let output2=(parseFloat(celsius.value)+273.15)
+    kelvin.value=parseFloat(output2.toFixed(2));
+};
+fahrenheit.oninput=() =>{
+    let output1=((parseFloat(fahrenheit.value)-32)*5)/9
+    celsius.value=parseFloat(output1.toFixed(2));
+    let output2=output1+273
+    kelvin.value=parseFloat(output2.toFixed(2))
+};
+kelvin.oninput=() =>{
+    let output1=((parseFloat(kelvin.value)-273.15))
+    celsius.value=parseFloat(output1.toFixed(2));
+    let output2=(parseFloat(output1)*9)/5+32
+    fahrenheit.value=parseFloat(output2.toFixed(2));
+
+};
